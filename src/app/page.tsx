675420cd6b1e22393b7cfede2453391a0cac3005
Fujiwara0805 <tiki4091@gmail.com>
2024-03-02 11:40:02 +0900

    ✨ Taskの全表示に成功

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
 import AddTask from "../components/layout/AddTask";
 import ToDoList from "../components/layout/ToDoList";
+import TASK from "@/types/type";
 
-export default function Home() {
+/* Task全取得 */
+const getTaskAll = async () => {
+  const res = await fetch("http://localhost:3000/api/task", {
+    cache: "no-store",
+  });
+  const TaskAllData: TASK[] = await res.json();
+  return TaskAllData;
+};
+
+const Home = async () => {
+  const tasks = await getTaskAll();
   return (
     <main className=" flex flex-col items-center justify-center min-h-screen py-4 bg-gray-200">
       <h1 className=" text-4xl text-gray-700 font-bold -mt-32">TODOアプリ</h1>
       <div className=" w-full max-w-xl mt-5 text-gray-700">
         <div className=" w-full px-8 py-6 bg-white shadow-lg rounded-md">
           <AddTask />
-          <ToDoList />
+          <ToDoList tasks={tasks} />
         </div>
       </div>
     </main>
   );
-}
+};
+
+export default Home;
--- src/components/layout/ToDoList.tsx
+++ src/components/layout/ToDoList.tsx
@@ -1,33 +1,29 @@
-import React from "react";
 import TASK from "@/types/type";
-import prisma from "@/lib/db";
+import React from "react";
 
-/* Task全取得 */
-const getTaskAll = async () => {
-  const res = await fetch("http://localhost:3000/api/task", {
-    cache: "no-store",
-  });
-  const TaskAllData: TASK[] = await res.json();
-  return TaskAllData;
-};
+interface TaskProps {
+  tasks: TASK[];
+}
 
-const ToDoList = () => {
+const ToDoList = ({ tasks }: TaskProps) => {
   return (
     <ul>
-      <li className="flex justify-between p-4 mt-2 bg-white border-l-4 border-blue-500 rounded-md shadow">
-        <span>散歩</span>
-        <div>
-          <button className=" text-green-500 mr-4">編集</button>
-          <button className=" text-red-500">削除</button>
-        </div>
-      </li>
-      <li className="flex justify-between p-4 mt-2 bg-white border-l-4 border-blue-500 rounded-md shadow">
-        <span>散歩</span>
-        <div>
-          <button className=" text-green-500 mr-4">編集</button>
-          <button className=" text-red-500">削除</button>
-        </div>
-      </li>
+      {tasks.map((task: TASK) => {
+        return (
+          <>
+            <li
+              id={task.id}
+              className="flex justify-between p-4 mt-2 bg-white border-l-4 border-blue-500 rounded-md shadow"
+            >
+              <span>{task.content}</span>
+              <div>
+                <button className=" text-green-500 mr-4">編集</button>
+                <button className=" text-red-500">削除</button>
+              </div>
+            </li>
+          </>
+        );
+      })}
     </ul>
   );
 };
