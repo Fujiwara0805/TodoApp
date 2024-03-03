cbce23f0d574efabf7018d19a377c59e90fbfabc
Fujiwara0805 <tiki4091@gmail.com>
2024-03-04 08:14:03 +0900

    ⚡️ タスク追加機能の実装

--- src/components/layout/AddTask.tsx
+++ src/components/layout/AddTask.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+"use client";
+import React, { use, useRef } from "react";
+
+/* Task追加 */
+const PostTask = async (content: string | undefined) => {
+  try {
+    const res = await fetch("http://localhost:3000/api/task", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ content }),
+    });
+    return res.json();
+  } catch (error) {
+    console.log("エラーが発生しました🔥", error);
+  }
+};
 
 const AddTask = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  /* ボタン押下でタスクを追加 */
+  const handleClick = async () => {
+    return await PostTask(inputRef.current?.value);
+  };
+
   return (
     <form className=" mb-4 space-y-3">
       <input
+        ref={inputRef}
         type="text"
         className=" w-full border px-4 py-2 rounded-lg focus:outline-none"
       />
-      <button className=" w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400  hover:scale-95 duration-200">
+      <button
+        className=" w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400  hover:scale-95 duration-200"
+        onClick={handleClick}
+      >
         Add Task
       </button>
     </form>
