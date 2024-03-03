a106fc7e9092825cdbf542a9431dc7c85ffe252e
Fujiwara0805 <tiki4091@gmail.com>
2024-03-03 22:02:51 +0900

    👽️ タスク追加用APIの実装

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -6,3 +6,14 @@ export const GET = async (req: Request, res: NextResponse) => {
   const posts = await prisma.post.findMany();
   return NextResponse.json(posts);
 };
+
+/* Task追加API */
+export const POST = async (req: Request, res: NextResponse) => {
+  const { content } = await req.json();
+  const task = await prisma.post.create({
+    data: {
+      content,
+    },
+  });
+  return NextResponse.json(task);
+};
