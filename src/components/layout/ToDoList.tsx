25b63ed7a724881cf356025520efe20316eb7c46
Fujiwara0805 <tiki4091@gmail.com>
2024-03-05 08:14:36 +0900

    ♻️ Taskコンポーネントへ移し替え

--- src/components/layout/ToDoList.tsx
+++ src/components/layout/ToDoList.tsx
@@ -1,5 +1,6 @@
 import TASK from "@/types/type";
 import React from "react";
+import Task from "./Task";
 
 interface TaskProps {
   tasks: TASK[];
@@ -11,16 +12,7 @@ const ToDoList = ({ tasks }: TaskProps) => {
       {tasks.map((task: TASK) => {
         return (
           <>
-            <li
-              id={task.id}
-              className="flex justify-between p-4 mt-2 bg-white border-l-4 border-blue-500 rounded-md shadow"
-            >
-              <span>{task.content}</span>
-              <div>
-                <button className=" text-green-500 mr-4">編集</button>
-                <button className=" text-red-500">削除</button>
-              </div>
-            </li>
+            <Task key={task.id} task={task} />
           </>
         );
       })}
