6f04e8bffba4e852e89db7be5803c4daf287397c
Fujiwara0805 <tiki4091@gmail.com>
2024-03-06 08:15:33 +0900

    🐛 編集用APIを移動

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -21,14 +21,3 @@ export const POST = async (req: Request, res: NextResponse) => {
   });
   return NextResponse.json(task);
 };
-
-/* Task編集用API */
-export const PUT = async (req: Request, { params }: ParamsProps) => {
-  const TaskId = await parseInt(params.TaskId);
-  const { content } = await req.json();
-  const post = await prisma.post.update({
-    data: { content },
-    where: { id: TaskId },
-  });
-  return NextResponse.json(post);
-};
